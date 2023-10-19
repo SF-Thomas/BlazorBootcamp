cd2e93524e149e0895e37371d2bf97c07070a7ce
Thomas Mertens <sf.thomas.mertens@gmail.com>
2023-10-19 12:41:16 +0200

    Individual Product Playground

---
+++ BlazorBootcamp.Models/LearnBlazor/DemoProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorBootcamp.Models.LearnBlazor
+{
+    public class DemoProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<DemoProductProperty> ProductProperties { get; set; } = new List<DemoProductProperty>();
+    }
+}
---
+++ BlazorBootcamp.Models/LearnBlazor/DemoProductProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBootcamp.Models.LearnBlazor
+{
+    public class DemoProductProperty
+    {
+        public int Id { get; set; }
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
